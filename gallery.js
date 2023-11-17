5e4c66bff2042062512c508ea3cdbec812143148
9r3i <luthfie@y7mail.com>
2023-11-18 00:13:29 +0700

    add sample gallery.js

---
+++ gallery.js
@@ -0,0 +1,15 @@
+const PROOM_GALLERY={
+  room:'', // lproom server or proxy if you have one
+  repo:'https://github.com/9r3i/proom/releases/download/', // host
+  ext:'jpg',     // file extention
+  interval:3,    // interval in second
+  password:'',   // if the server has one
+  previous:'<<', // previous button
+  next:'>>',     // next button
+  gallery:{
+    "001":{ // must be 3 padded zero of stage as well as level
+      title:"palestine children", // * required; stage title
+      level:15, // * required; how many level on this stage
+    }
+  }
+};
